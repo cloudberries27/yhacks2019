6e06aa62c6d45b90193ff3d938614127893f450b
sv1419 <sv1419@nyu.edu>
2019-10-26 16:55:32 -0400

    Implemented API for geolocation

---
+++ gpsTrack.js
@@ -0,0 +1,14 @@
+var IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
+const apiKey = '951cd492488f476e8cdbac21c9916f6e';
+
+var ipgeolocationApi = new IPGeolocationAPI(apiKey, false);
+
+// Function to handle response from IP Geolocation API
+function handleResponse(json) {
+    console.log(json);
+}
+
+var GeolocationParams = require('ip-geolocation-api-javascript-sdk/GeolocationParams.js');
+
+// Get complete geolocation for the calling machine's IP address
+ipgeolocationApi.getGeolocation(handleResponse);
