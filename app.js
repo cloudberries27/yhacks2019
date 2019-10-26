540f177d8b5379edb459006daadb55baceb7f22a
coudberries27 <ccrod2298@gmail.com>
2019-10-26 19:44:14 -0400

    .gitignore is now working

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+
+  const config = {
+    apiKey: 'AIzaSyCdbaaXzEJZloTpyb242vvDvn026jQnDyo',
+    authDomain: 'diddley-squad.firebaseapp.com',
+    databaseURL: 'https://diddley-squad.firebaseio.com',
+    projectId: 'diddley-squad',
+    storageBucket: 'diddley-squad.appspot.com',
+    messagingSenderId: '765373219024',
+    appId: '1:765373219024:web:e63c1a883f3d7ba67c9ed5',
+  };
+  firebase.initializeApp(config);
+
+})
