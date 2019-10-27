f8696b8615b73a6df50a8d4a396595c3410d2c21
sv1419 <sv1419@nyu.edu>
2019-10-27 04:49:08 -0400

    Created Panic notification

--- public/send_sms.js
+++ public/send_sms.js
@@ -2,17 +2,10 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 
-
-function send_sms(){
-  const accountSid = 'AC5e2c3a4b2bdecf513fedce64b4a88e69';
-  const authToken = '8d24fed32aaac7db392d4a9547ff9747';
-  const client = require('twilio')(accountSid, authToken);
-
-  client.messages
-    .create({
-      body: 'Hi, Claudia! This is Jamie from YHack, and Im on my way to assist you!',
-      from: '+15594782867',
-      to: '+19146400721'
-    })
-    .then(message => console.log(message.sid));
+function sendText(){
+    window.alert("We have alerted your friends that you are in danger and have pushed them your current location. If this an emergency, please contact the police.")
+}
+var smsSend = document.getElementById('sendMessageButton');
+if(smsSend){
+  smsSend.addEventListener('click', sendText, false);
 }
