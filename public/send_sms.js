a571bbf8cd604cb815b0daf962ba00f933f225d7
sv1419 <sv1419@nyu.edu>
2019-10-27 05:43:59 -0400

    working on friend selections

--- public/send_sms.js
+++ public/send_sms.js
@@ -3,9 +3,15 @@
 // DANGER! This is insecure. See http://twil.io/secure
 
 function sendText(){
-    window.alert("We have alerted your friends that you are in danger and have pushed them your current location. If this an emergency, please contact the police.")
+    window.alert("We have alerted your friends that you are in danger and have pushed them your current location. IF THIS IS AN EMERGENCY, PLEASE CONTACT THE POLICE.")
 }
 var smsSend = document.getElementById('sendMessageButton');
 if(smsSend){
   smsSend.addEventListener('click', sendText, false);
+}
+
+var ending = document.getElementById('endParty')
+ending.onclick = endPart
+function endPart(){
+    window.alert("You have requested to end your party. Waiting on other members to confirm")
 }
